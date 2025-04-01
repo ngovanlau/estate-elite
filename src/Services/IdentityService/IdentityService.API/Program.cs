0d539cf533455cb22e83491cffe7d219561b7180
ngovanlau <ngovanlau2003@gmail.com>
2025-04-01 22:40:17 +0700

    Create nginx successfully

--- src/Services/IdentityService/IdentityService.API/Program.cs
+++ src/Services/IdentityService/IdentityService.API/Program.cs
@@ -32,7 +32,7 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection(); enable when use https
 app.UseAuthorization();
 app.MapControllers();
 
