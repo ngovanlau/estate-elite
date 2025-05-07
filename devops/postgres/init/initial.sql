6642309cea9e290a791566892d5c652cf7628e7e
ngovanlau <ngovanlau2003@gmail.com>
2025-05-07 16:38:55 +0700

    Update docker compose

--- devops/postgres/init/initial.sql
+++ devops/postgres/init/initial.sql
@@ -1,3 +1,17 @@
-CREATE USER lau WITH PASSWORD 'lau2962003';
-CREATE DATABASE estate_elite;
-GRANT ALL PRIVILEGES ON DATABASE estate_elite TO lau;
+DO $$
+BEGIN
+-- Create user if not exists
+IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'lau') THEN
+    CREATE USER lau WITH PASSWORD 'lau2962003';
+END IF;
+
+-- Create database if not exists
+IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'estate_elite') THEN
+    CREATE DATABASE estate_elite WITH OWNER lau;
+END IF;
+END
+$$;
+
+-- Connect to the estate_elite database
+\c estate_elite
+-- Additional initialization scripts can go here
